df9980577772255b133b6864f4164d7011b73432
KadirOzerOzturk <ozturkkadirozer@gmail.com>
2025-03-01 20:10:51 +0300

    update database configuration and add Docker Compose setup

--- internal/database/database.go
+++ internal/database/database.go
@@ -13,12 +13,13 @@ var DB *gorm.DB
 
 func Connection() *gorm.DB {
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=Europe/Istanbul",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s timezone=Europe/Istanbul",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
 		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSL"),
 	)
 	connection, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
